c91d920aa585a8586b333f5415f924933c8cbcd4
Fitria Khasanah <fitriaksnh777@gmail.com>
2024-05-07 21:41:00 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -1,22 +1,21 @@
+/* eslint-disable linebreak-style */
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
-https://eslint.org/docs/latest/rules/no-trailing-spaces;
 
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
-        cors: {
-          origin: ['*'],
-        },
+      cors: {
+        origin: ['*'],
       },
+    },
   });
- 
+
   server.route(routes);
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
- 
- 
+
 init();
